feb9ba70556bb112ff1c56b10b994c6d6ab0f461
Jawad Ul Hassan <jawadulhassan50@gmail.com>
2020-03-22 18:58:59 +0500

    epic boilerplate updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { Provider } from "react-redux";
-
-import store from "./store";
-import Home from "./Home";
 
 export default () => (
-  <Provider store={store}>
-    <Home />
-  </Provider>
+  <h1>Hello World!</h1>
 );
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import helloWorld from './helloWorld';
-
-export default combineReducers({
-  helloWorld,
-});
